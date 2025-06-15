b198edc0d6d90d6e2cbb12306a0c64a826267e8b
henriquemtn <henriquemartinsilveira2@gmail.com>
2025-06-15 04:20:32 -0300

    first commit

---
+++ src/main/java/com/auth/api/springsecurity/entities/Role.java
@@ -0,0 +1,42 @@
+package com.auth.api.springsecurity.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long roleId;
+
+    private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public enum Values {
+        BASIC,
+        ADMIN;
+
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+    }
+}
---
+++ src/main/java/com/auth/api/springsecurity/entities/Tweet.java
@@ -0,0 +1,53 @@
+package com.auth.api.springsecurity.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant timestamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+}
